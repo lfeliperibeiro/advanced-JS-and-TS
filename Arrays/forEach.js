bcef5f6d66cf5cb68ee03861e235e71a02c1c215
Felipe Ribeiro <piporibeiro@gmail.com>
2022-05-26 18:06:57 -0300

    forEach

---
+++ Arrays/forEach.js
@@ -0,0 +1,5 @@
+// forEach é uma função que percorre um array e executa uma função para cada elemento.
+
+const numbers = [1, 2, 5, 9, 10, 15, 20];
+
+numbers.forEach(number => console.log(number));
